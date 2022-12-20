eb69b78bc62f7eb963b95a7720c60250b077369b
shuvokoiri0@gmail.com <shuvokoiri0@gmail.com>
2022-12-21 01:50:17 +0600

    set for depltow

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,8 +1,6 @@
 import { Navigate } from "react-router-dom";
 import { useContext } from "react";
-import { UserContext } from "../assets/userContext";
 const Protected = ({ children }) => {
-  const { value } = useContext(UserContext);
   if (!localStorage.getItem("admin-token")) {
     return <Navigate to="/" replace={true} />;
   }
